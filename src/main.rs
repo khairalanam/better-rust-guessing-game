9b012231cf0567acbae330d27654188ff0bca039
Khair Alanam <anaambasheer@gmail.com>
2023-09-03 11:24:51 +0530

    update guessing game

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,40 @@ use rand::Rng;
 
 fn main() {
     let secret_number: i32 = rand::thread_rng().gen_range(1..101);
+    let mut i: i32 = 0;
+    const A: i32 = 100_000_000;
 
-    loop {
+    println!("{}", A);
+
+    while i < 5 {
+        println!("Guess {}", i + 1);
         println!("Guess the number between 1 and 100: ");
         let mut user_input: String = String::new();
 
         io::stdin().read_line(&mut user_input).expect("Some Input Error.");
 
-        let user_input: i32 = user_input.trim().parse().expect("Not an integer");
+        let user_input: i32 = match user_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid Input! Try again!");
+                continue;
+            },
+        };
 
         if user_input < secret_number {
             println!("Too small!");
         } else if user_input > secret_number {
             println!("Too big!");
         } else {
-            println!("You guessed it! It's {}!", secret_number);
             break;
         }
+
+        i += 1;
+    }
+
+    if i == 5 {
+        println!("You have lost! The number was {}!", secret_number);
+    } else {
+        println!("You guessed it! It's {}!", secret_number);
     }
 }
